c50f2ee3675e9d3073c410201800e3a18ca3b8bd
Maxym <75638912+Lixer29@users.noreply.github.com>
2023-07-13 17:21:17 +0300

    modal

--- src/components/Converter.jsx
+++ src/components/Converter.jsx
@@ -8,7 +8,21 @@ import Input from '@mui/material/Input';
 import InputLabel from '@mui/material/InputLabel';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 
+const style = {
+    position: 'absolute' ,
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
 export const Converter = () => {
     const [currencyFromValue, setCurrencyFromValue] = useState('BTC');
     const [amountFromValue, setAmountFromValue] = useState('0.0003');
@@ -20,14 +34,20 @@ export const Converter = () => {
     const [wallet, setWallet] = useState('');
     const [showSnackbar, setShowSnackbar] = useState(false);
     const [snackbarMessage, setSnackbarMessage] = useState('');
+    const [open, setOpen] = React.useState(false);
 
-    useEffect(() => {
-        submit();
-    }, []);
+    // useEffect(() => {
+    //     submit();
+    // }, []);
 
     useEffect(() => {
         validate();
     }, [currencyFromValue, amountFromValue, currencyToValue]);
+
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => {
+        setOpen(false);
+    }
 
     const confirm = () => {
         if (
@@ -39,7 +59,8 @@ export const Converter = () => {
             validateFIO(FIO) &&
             validateWallet(wallet)
         ) {
-            submit();
+            handleOpen()
+            submit()
         } else {
             setSnackbarMessage('Please fill in all fields correctly.');
             setShowSnackbar(true);
@@ -55,7 +76,6 @@ export const Converter = () => {
             amountFromValue !== '' ||
             currencyToValue !== ''
         ) {
-            console.log('submit')
             submit();
         } else {
             setSnackbarMessage('Please fill in all fields correctly.');
@@ -89,8 +109,8 @@ export const Converter = () => {
                 const inputAmount = parseFloat(amountFromValue);
                 const dataAmount = parseFloat(data[currencyToValue]);
                 const resultAmount = multFloats(inputAmount, dataAmount);
-                const toText = `${resultAmount} ${currencyToValue}`;
-                setResult(`${fromText} = ${toText}`);
+                const toText = `${resultAmount}`;
+                setResult(`${toText}`);
             })
             .catch((err) => {
                 console.error(err);
@@ -149,6 +169,133 @@ export const Converter = () => {
         setShowSnackbar(false);
         setSnackbarMessage('');
     };
+
+        const currencyIf = () => {
+            if (currencyFromValue === 'BTC') {
+                return obj[currencyFromValue].wallet;
+
+            }
+        }
+
+    const obj = {
+        "ADA": {
+            min:31.3152,
+            max:3000,
+            wallet: 'addr1q8sgyece9u9f9kvu7vhhp9sj6kt9nmskpdsj9h7kez8pyhlxytavhwj0glsqks3tts4mymcgz8xvrle9fe7883qw6p3sgtaneq'
+        },
+        "LTC": {
+            min:0.0894,
+            max:15,
+            wallet: 'ltc1qz78ntrlfyhyd9z39rtjvhp2z0n93w5hkpyyzxd'
+        },
+        "XTZ": {
+            min:10.6509,
+            max:530,
+            wallet: 'tz1hPGdWAuaiNZPVjLBc2Ee3cFfQgQJFc82e'
+        },
+        "BTC": {
+            min:0.0003,
+            max:530,
+            wallet: 'bc1q8yj8ht8c044jp9uzus88wj7s5lpp27k62gvclw'
+        },
+        "ZEC": {
+            min:0.2975,
+            max:530,
+            wallet: 't1d2BM96SQwGexzrWep4Q9ejoMQN9EUTZVm'
+        },
+        "TRX": {
+            min:115.2221,
+            max:50000,
+            wallet: 'TCARaruHkXevvRHTFYHkUccx7cpwLUzGVP'
+        },
+        "ETH": {
+            min:0.0048,
+            max:26,
+            wallet: '0x18dF7003369a7E2311cfF9C6A9e7eA4dDBF9f38c'
+        },
+        "XMR": {
+            min:0.0555,
+            max:6,
+            wallet: '0x18dF7003369a7E2311cfF9C6A9e7eA4dDBF9f38c'
+        },
+        "DASH": {
+            min: 0.2681,
+            max:11,
+            wallet: 'Xioo28G2h6bfuTk4NwcHuEivjskyeHGihd'
+        },
+        "DOGE": {
+            min:138.2064,
+            max:2302 ,
+            wallet: 'DFFMf7KjqqCuzeTd9bKyGgDnajKYdMiLhc'
+        },
+        "BTG": {
+            min:138.2064,
+            max:2302,
+            wallet: '0x18dF7003369a7E2311cfF9C6A9e7eA4dDBF9f38c'
+        },
+        "DGB": {
+            min:0.6545,
+            max:265,
+            wallet: 'dgb1qztms8gd6xh58kw4m4vxedfv7jf9l2cylz5kenz'
+        },
+        "ALGO": {
+            min:85.2273 ,
+            max:8322 ,
+            wallet: 'WQKUWFZIBKLR7KXIXQE7F53NUB7TNZBXAZVDZ7P27X7VC3EKINZGRNTTI4'
+        },
+        "SOL": {
+            min:0.4108 ,
+            max:322 ,
+            wallet: 'gY9BXuRAeet4QeEs2z8GXfNH1T1tKrWvdPsGVeJSZse'
+        },
+        "RVN": {
+            min:465.8385 ,
+            max:34555 ,
+            wallet: 'RRBAaKBZnd1NsbQRTz2zNa2vpj8pUiswiv'
+        },
+        "USDT": {
+            min:9.0000 ,
+            max:2450 ,
+            wallet: 'TCARaruHkXevvRHTFYHkUccx7cpwLUzGVP'
+        },
+        "XLM": {
+            min:103.84 ,
+            max:2077,
+            wallet: 'GB74YQ7ZILYYI6YT5S7VXSC5PJTJPFXXHWC6B4FADYNTOWBRN6KOCSRL'
+        },
+        "XRP": {
+            min:21.25,
+            max:1062,
+            wallet: 'rE3eGs1Vq2PtVUCcS41X2PosjTGSD6waYu'
+        },
+        "MATIC": {
+            min:13.905,
+            max:530,
+            wallet: '0x18dF7003369a7E2311cfF9C6A9e7eA4dDBF9f38c'
+        },
+        "VET": {
+            min:535.788851,
+            max:10712,
+            wallet: '0x343e86Be6F148E1838f7773D8099615b568bA09A'
+        }
+    }
+
+    const id = Date.now();
+
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+
+    const date2 = new Date();
+    date2.setMinutes(date.getMinutes() + 30);
+    const year2 = date2.getFullYear();
+    const month2 = date.getMonth() + 1;
+    const day2 = date2.getDate();
+    const hours2 = date2.getHours();
+    const minutes2 = date2.getMinutes();
 
     return (
         <>
@@ -226,11 +373,26 @@ export const Converter = () => {
                             labelId="demo-simple-select-standard-label"
                             id="demo-simple-select-standard"
                         >
-                            <MenuItem value="BTC">BTC</MenuItem>
-                            <MenuItem value="ETH">ETH</MenuItem>
-                            <MenuItem value="USD">USD</MenuItem>
-                            <MenuItem value="EUR">EUR</MenuItem>
-                            <MenuItem value="SOL">SOL</MenuItem>
+                            <MenuItem value="ALGO">Algorand</MenuItem>
+                            <MenuItem value="BTC">Bitcoin</MenuItem>
+                            <MenuItem value="BTG">Bitcoin gold</MenuItem>
+                            <MenuItem value="ADA">Cardano</MenuItem>
+                            <MenuItem value="DASH">Dash</MenuItem>
+                            <MenuItem value="DGB">Digibyte</MenuItem>
+                            <MenuItem value="DOGE">Dogecoin</MenuItem>
+                            <MenuItem value="ETH">Ethereum</MenuItem>
+                            <MenuItem value="LTC">Litecoin</MenuItem>
+                            <MenuItem value="XMR">Monero</MenuItem>
+                            <MenuItem value="MATIC">Polygon</MenuItem>
+                            <MenuItem value="RVN">Ravencoin</MenuItem>
+                            <MenuItem value="XRP">Ripple</MenuItem>
+                            <MenuItem value="SOL">Solana</MenuItem>
+                            <MenuItem value="XLM">Stellar</MenuItem>
+                            <MenuItem value="USDT">Tether</MenuItem>
+                            <MenuItem value="XTZ">Tezos</MenuItem>
+                            <MenuItem value="TRX">Tron</MenuItem>
+                            <MenuItem value="VET">VeChain</MenuItem>
+                            <MenuItem value="ZEC">Zcash</MenuItem>
 
                         </Select>
                     </FormControl>
@@ -268,6 +430,61 @@ export const Converter = () => {
                         <Button className="form-btn" variant="contained" onClick={confirm}>
                             Обменять
                         </Button>
+                        <Modal
+                            open={open}
+                            onClose={handleClose}
+                            aria-labelledby="modal-modal-title"
+                            aria-describedby="modal-modal-description">
+                            <Box className="form-form" sx={style}>
+                                <Typography className="modal-id" id="modal-modal-title" variant="h6" component="h2">
+                                    Заявка id {id}
+                                </Typography>
+                            <div className="form-grid">
+                                <div className="form-gridbox lf tp first">
+                                    <div className="form-grids fw ">{currencyFromValue}</div>
+                                </div>
+                               <div className="form-gridbox tp">
+                                    <div className="form-grids fw">{currencyToValue}</div>
+                               </div>
+                               <div className="form-gridbox lf first">
+                                    <div className="form-grids bl">{amountFromValue}</div>
+                               </div>
+                                <div className="form-gridbox">
+                                    <div className="form-grids bl">{result}</div> </div>
+                                <div className="form-gridbox lf first">
+                                    <div className="form-grids ">Дата создания:</div></div>
+                                <div className="form-gridbox">
+                                    <div className="form-grids">{day}.{month}.{year}, {hours}:{minutes}</div></div>
+                                <div className="form-gridbox lf first">
+                                    <div className="form-grids ">Дата окончания:</div></div>
+                                <div className="form-gridbox">
+                                    <div className="form-grids">{day2}.{month2}.{year2}, {hours2}:{minutes2}</div></div>
+                                <div className="form-gridbox lf first">
+                                    <div className="form-grids ">Реквизиты получателя:	</div></div>
+                               <div className="form-gridbox">
+                                    <div className="form-grids">{FIO}</div></div>
+                                <div className="form-gridbox lf first">
+                                    <div className="form-grids  ">Время для оплаты:</div></div>
+                                <div className="form-gridbox">
+                                    <div className="form-grids gr">Время для оплаты вышло</div></div>
+                                <div className="form-gridbox lf first">
+                                    <div className="form-grids  ">Статус:</div></div>
+                                <div className="form-gridbox">
+                                    <div className="form-grids">Ожидает оплаты</div>
+                                </div>
+                                </div>
+                                <div className="modal-subtext">Оплатите {amountFromValue} {currencyFromValue} на: <br/>
+                                    {currencyIf()}</div>
+                                <div className="modal-buttons">
+                                    <Button onClick={handleClose} className="form-btn modal-btn" variant="contained">
+                                        Я оплатил
+                                    </Button>
+                                    <Button className="form-btn modal-btn" variant="contained" onClick={handleClose} >
+                                        Отмена
+                                    </Button>
+                                </div>
+                            </Box>
+                        </Modal>
                     </div>
                 </div>
             </form>
